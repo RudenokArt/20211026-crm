b9b783f247f6b739bfa163cfd4ce94f89ce858dc
RudenokArt <rudenokart@yandex.ru>
2021-10-28 00:36:18 +0300

    login page

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- JQUERY -->
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <!-- BOOTSTRAP -->
+  <link rel="stylesheet" 
+  href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" 
+  integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+  crossorigin="anonymous">
+  <script 
+  src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" 
+  integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" 
+  crossorigin="anonymous"></script>
+  <!-- FONT AWESSOME -->
+  <script src="https://use.fontawesome.com/e8a42d7e14.js"></script>
+  <!-- MASKEDINPUT -->
+  <script src="https://cdn.jsdelivr.net/npm/jquery.maskedinput@1.4.1/src/jquery.maskedinput.min.js" type="text/javascript"></script>
+  <!-- ========= -->
+	<title>Document</title>
+</head>
+<body>
--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-crm
+<?php get_header();?>
+
+<?php if (is_user_logged_in()): ?>
+	<?php include_once 'layout/main.php';?>
+<?php else: ?>
+	<?php include_once 'layout/login.php';?>
+<?php endif ?>
+
+<?php get_footer(); ?>
