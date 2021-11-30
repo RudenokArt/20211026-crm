fc9301dcaafc68ec34b4bbeb62b77dde129c0888
rudenokart <rudenokart@yandex.ru>
2021-11-30 09:42:53 +0300

    order-printform bugfix

--- single.php
+++ single.php
@@ -8,29 +8,32 @@ $current_order = post_data(get_post()->ID);
     <div class="col-12">
       <table class="order_print">
         <tr>
-          <th>
-            Заказ(<?php echo $order_status[$current_order['meta']['order_status'][0]];?>):
-          </th>
-          <th>
-            <?php echo $current_order['meta']['order_date'][0]; ?>
-          </th>
-          <th>
-            <?php echo $adress_arr[$current_order['data']->post_title]; ?>
-          </th>
+          <th>Заказ: </th>
+          <td><?php echo  get_post()->ID ?></td>
         </tr>
         <tr>
-          <th>Контакты заказчика:</th>
-          <th>
-            <?php echo $current_order['meta']['customer_adress'][0]; ?>
-          </th>
-          <th>
-            <?php echo $current_order['meta']['customer_phone'][0]; ?>
-          </th>
+          <th>Офис:</th>
+          <td><?php echo $adress_arr[$current_order['data']->post_title]; ?></td>
         </tr>
         <tr>
-          <td colspan="3">
-            <?php echo $current_order['data']->post_content; ?>
-          </td>
+          <th>Дата: </th>
+          <td><?php echo $current_order['meta']['order_date'][0]; ?></td>
+        </tr>
+        <tr>
+          <th>Статус: </th>
+          <td><?php echo $order_status[$current_order['meta']['order_status'][0]];?></td>
+        </tr>
+        <tr>
+          <th>Адрес заказчика: </th>
+          <td><?php echo $current_order['meta']['customer_adress'][0]; ?></td>
+        </tr>
+        <tr>
+          <th>Телефон заказчика</th>
+          <td><?php echo $current_order['meta']['customer_phone'][0]; ?></td>
+        </tr>
+        <tr>
+          <th>Комментарий:</th>
+          <td><?php echo $current_order['data']->post_content; ?></td>
         </tr>
       </table>
     </div>
